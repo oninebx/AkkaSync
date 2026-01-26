e40428c69ed041f8708e80cd2cd0e31e56d8b420
Ryan Xu <cxu@example.com>
2026-01-26 17:37:55 +1300

    ci: publish docker image to repository package

--- publish/AkkaSync.Publish/Program.cs
+++ publish/AkkaSync.Publish/Program.cs
@@ -2,7 +2,6 @@
 
 var config = PublishHelper.LoadConfig();
 
-var root = Directory.GetCurrentDirectory();
 var releaseRoot = Path.Combine(AppContext.BaseDirectory, "../../../..", "releases", config.ReleaseVersion);
 Directory.CreateDirectory(releaseRoot);
 
@@ -10,7 +9,7 @@ var templateRoot = Path.Combine(AppContext.BaseDirectory, "Templates");
 PublishHelper.CopyDirectory(templateRoot, releaseRoot);
 
 var envFilePath = Path.Combine(releaseRoot, ".env");
-var envContent = $"AKKASYNC_VERSION={config.ReleaseVersion}";
+var envContent = $"AKKASYNC_VERSION={config.AkkaSyncVersion}";
 File.WriteAllText(envFilePath, envContent);
 
 Console.WriteLine($".env file generated at {envFilePath}");
@@ -18,8 +17,8 @@ Console.WriteLine($".env file generated at {envFilePath}");
 var exampleRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, config.ExamplesRoot, config.Demo));
 
 PublishHelper.CopyDirectory(Path.Combine(exampleRoot, "configs"), Path.Combine(releaseRoot, "pipelines"));
-PublishHelper.CopyDirectory(Path.Combine(exampleRoot, "input"), Path.Combine(releaseRoot, "data", "input"));
-PublishHelper.CopyDirectory(Path.Combine(exampleRoot, "output"), Path.Combine(releaseRoot, "data", "output"));
+PublishHelper.CopyDirectory(Path.Combine(exampleRoot, "input"), Path.Combine(releaseRoot, "data", config.Demo, "input"));
+PublishHelper.CopyDirectory(Path.Combine(exampleRoot, "output"), Path.Combine(releaseRoot, "data", config.Demo, "output"));
 
 
 
--- publish/AkkaSync.Publish/README.md
+++ publish/AkkaSync.Publish/README.md
@@ -1,5 +1,14 @@
-git tag v0.1.0-demo.1
-git push origin v0.1.0-demo.1
+## Publish AkkaSync Docker
+git tag v0.1.0
+git push origin v0.1.0
 
+## Publish AkkaSync Demo Assets
+git tag assets-0.1.0
+git push origin assets-0.1.0
+
+## Delete tag
 git tag -d v0.1.0-demo.1
 git push origin --delete v0.1.0-demo.1
+
+## Run locally
+dotnet run --project publish/AkkaSync.Publish/AkkaSync.Publish.csproj --configuration Release
